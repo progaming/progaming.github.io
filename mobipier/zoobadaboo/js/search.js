985aefb081798d1e24e29a409e5680b724969871
Weerawee Jintaya <weerawee@progaming.co.th>
2015-07-14 10:31:45 +0700

    [zoobadaboo] add search vip

--- mobipier/zoobadaboo/js/search.js
+++ mobipier/zoobadaboo/js/search.js
@@ -10,9 +10,74 @@
 		searchCtrl.allUserCnt = 0;
 		$scope.searchInfo = new Array();
 		searchCtrl.userInfoArr = new Array();
-		FindAllUser();
 		searchCtrl.userShowPerPage = 100;
+		searchCtrl.vipIdArr = new Array();
+		searchCtrl.isFindVip = false;
+		var lessThan = null;
+		//FindAllUser();
+		LoadVipId();
 		//------------
+		
+		function LoadVipId()
+		{
+			var query = new Parse.Query("VIP");
+		 	query.descending("updatedAt");
+		 	if(lessThan != null)
+		 		query.lessThan("updatedAt", lessThan);
+		 	query.limit(1000);
+			query.find({
+				  success: function(objects) {
+					console.log("object = " + objects.length);
+					for(var i = 0; i < objects.length; i++)
+					{
+						var _active = objects[i].get("active");
+						var uid = objects[i].get("uid");
+						if(objects[i].has("active") && _active == true)
+						{
+							if(objects[i].has("uid"))
+							{
+								//console.log("add id" + uid);
+								searchCtrl.vipIdArr.push(uid);
+							}
+						}
+						else
+						{
+							if(objects[i].has("EndDate"))
+							{
+								var _endDate = objects[i].get("EndDate");
+								var _date = new Date();
+								if( _endDate > _date )
+								{
+									//console.log("add id" + uid + " _date: " + _date.toString() + " _endDate: " + _endDate.toString());
+									if(objects[i].has("uid"))
+									{
+										searchCtrl.vipIdArr.push(uid);
+									}	
+								}
+								else
+								{
+									//console.log("don't add id" + uid + " _date: " + _date.toString() + " _endDate: " + _endDate.toString());
+								}
+							}
+						}
+						
+						lessThan = objects[i].updatedAt;
+					}
+					
+					if(objects.length == 0)
+					{
+						FindAllUser();
+					}else
+					{
+						LoadVipId();
+					}
+					
+				  },
+				  error: function(error) {
+					$scope.message = "Can't search User.";
+				  }
+			  });
+		}
 		
 		function FindAllUser(){
 			console.log("FindAllUser");
@@ -32,6 +97,12 @@
 							searchCtrl.uidArr.push(_uid);
 						}
 						
+						var _isVip = "No";
+						if(searchCtrl.vipIdArr.indexOf(_uid) != -1)
+						{
+							_isVip = "Yes";
+						}
+						
 						var createdtime = objects[i].createdAt;
 						var createdtimeStr = GetTimeString(createdtime);
 						var updatetime = objects[i].updatedAt;
@@ -48,7 +119,8 @@
 						scoreChar: objects[i].get("scoreCharacter"),
 						count: objects[i].get("count"),
 						android: objects[i].get("Android"),
-						ios: objects[i].get("iOS")});
+						ios: objects[i].get("iOS"),
+						vip:_isVip});
 
 						searchCtrl.userInfoArr.push(searchCtrl.allUserInfoArr[searchCtrl.allUserCnt]);
 						if(searchCtrl.allUserCnt < searchCtrl.userShowPerPage)
@@ -109,7 +181,16 @@
 		
 		this.searchUser = function()
 		{
-			console.log("searchUser");
+			if( searchCtrl.isFindVip &&searchCtrl.vipIdArr.length == 0)
+			{
+				$scope.searchInfo = new Array();
+				searchCtrl.userInfoArr = new Array();
+				$scope.needMorePage = false;
+				$scope.isSearch = true;
+				$scope.$apply();
+				return;
+			}
+			console.log("searchUser isFindVIP: " + searchCtrl.isFindVip);
 			 var query1 = new Parse.Query("UserInfo");
 			 query1.contains("first_name", $scope.textsearch);
 			 
@@ -119,6 +200,13 @@
 			 var query3 = new Parse.Query("UserInfo");
 			 query3.contains("email", $scope.textsearch);
 			 
+			if(searchCtrl.isFindVip)
+			{
+				query1.containedIn("uid", searchCtrl.vipIdArr);
+				query2.containedIn("uid", searchCtrl.vipIdArr);
+				query3.containedIn("uid", searchCtrl.vipIdArr);
+			}
+			 
 			 var compoundQuery = Parse.Query.or(query1, query2, query3);
 				  compoundQuery.find({
 					  success: function(objects) {
@@ -127,6 +215,13 @@
 						console.log("object = " + objects.length);
 						for(var i = 0; i < objects.length; i++)
 						{
+							var _uid = objects[i].get("uid");
+							var _isVip = "No";
+							if(searchCtrl.vipIdArr.indexOf(_uid) != -1)
+							{
+								_isVip = "Yes";
+							}
+							
 							var createdtime = objects[i].createdAt;
 							var createdtimeStr = GetTimeString(createdtime);
 							var updatetime = objects[i].updatedAt;
@@ -136,18 +231,18 @@
 							lastname:objects[i].get("last_name"), 
 							email:objects[i].get("email"), 
 							createdAt:createdtimeStr,
+							updateAt:updatetimeStr,
 							highscore: objects[i].get("highscore"),
 							highscoreChar: objects[i].get("highscoreCharacter"),
 							score: objects[i].get("score"),
 							scoreChar: objects[i].get("scoreCharacter"),
 							count: objects[i].get("count"),
 							android: objects[i].get("Android"),
-							ios: objects[i].get("iOS")});
+							ios: objects[i].get("iOS"),
+							vip:_isVip});
 							if(i < searchCtrl.userShowPerPage)
 							{
-								$scope.searchInfo.push({firstname:objects[i].get("first_name"), 
-								lastname:objects[i].get("last_name"), 
-								email:objects[i].get("email"), createdAt:createdtimeStr});
+								$scope.searchInfo.push(searchCtrl.userInfoArr[i]);
 							}
 						}
 						$scope.userdataPageNum = new Array();
