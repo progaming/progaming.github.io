6299b5e99dbba847df866e53edcca0f2a48548e8
weerawee <weerawee@progaming.co.th>
2015-11-27 19:07:04 +0700

    [zpellracing] add Date

--- zpellracing/js/search.js
+++ zpellracing/js/search.js
@@ -17,6 +17,7 @@
 		var isNeedAllUser = false;
 		//FindAllUser();
 		GetCount();
+		SetDateArray();
 		//------------
 		
 		function GetCount()
@@ -203,6 +204,119 @@
 	   		}
 			
 		}
+		
+		function SetDateArray()
+		{
+			searchCtrl.dayArr = new Array();
+			var date = new Date();
+			var startDate = new Date(2015,11,27);
+			var cnt = 0;
+			while(date.getDate() == startDate.getDate() && date.getMonth() == startDate.getMonth() &&
+				date.getFullYear() == startDate.getFullYear())
+				{
+					date = new Date();
+					date.setDate(date.getDate()+ cnt);
+					searchCtrl.dayArr.push({showDate: formatDate(date),
+											realDate: date});
+					cnt--;
+					
+				}
+			if(searchCtrl.dayArr.length == 0)
+			{
+				date = new Date();
+				searchCtrl.dayArr.push({showDate: formatDate(date),
+										realDate: date});
+			}
+		}
+		
+		function formatDate(date) {
+			var d = new Date(date),
+				month = '' + (d.getMonth() + 1),
+				day = '' + d.getDate(),
+				year = d.getFullYear();
+		
+			if (month.length < 2) month = '0' + month;
+			if (day.length < 2) day = '0' + day;
+		
+			return [ day, month, year].join('-');
+		}
+		
+		function GetScoreClass(date)
+		{
+			var d = new Date(date);
+			var dateStr = d.getDate() + "";
+			var monthStr = (d.getMonth()+1) + "";
+			var yearStr = d.getFullYear();
+			if(dateStr.length < 2) { dateStr = "0" + dateStr; }
+			if(monthStr.length < 2) { monthStr = "0" + monthStr; }
+			return "Score" + dateStr + monthStr + yearStr;
+		}
+		
+		this.gotoDate = function(date)
+		{
+			console.log("gotoDate");
+			var classStr;
+			var d = new Date(date);
+			var today = new Date();
+			if(d.getUTCDate() == today.getUTCDate() && d.getUTCMonth() == today.getUTCMonth() &&
+			   d.getUTCFullYear() == today.getUTCFullYear())
+			   {
+				   classStr = "Score";
+			   }
+			   else
+			   {
+				   classStr = GetScoreClass(date);
+			   }
+				var query = new Parse.Query(classStr);
+				query.descending("updatedAt");
+				if(searchCtrl.lessThan != null)
+					query.lessThan("updatedAt", searchCtrl.lessThan);
+				query.descending("score");
+				query.limit(1000);
+				query.find({
+					success: function(objects) {
+						console.log("object = " + objects.length);
+						for(var i = 0; i < objects.length; i++)
+						{
+							var _uid = objects[i].get("uid");
+							if(searchCtrl.uidArr.indexOf(_uid) == -1)
+							{
+								searchCtrl.uidArr.push(_uid);
+							}
+							
+							var createdtime = objects[i].createdAt;
+							var createdtimeStr = GetTimeString(createdtime);
+							var updatetime = objects[i].updatedAt;
+							var updatetimeStr = GetTimeString(updatetime);
+							
+							searchCtrl.allUserInfoArr.push({firstname:objects[i].get("first_name"), 
+							lastname:objects[i].get("last_name"), 
+							createdAt:createdtimeStr,
+							updateAt:updatetimeStr,
+							score: objects[i].get("score"),
+							uid:objects[i].get("uid")});
+	
+							searchCtrl.userInfoArr.push(searchCtrl.allUserInfoArr[searchCtrl.allUserCnt]);
+							if(searchCtrl.allUserCnt < searchCtrl.userShowPerPage)
+							{
+								$scope.searchInfo.push(searchCtrl.userInfoArr[searchCtrl.allUserCnt]);
+							}
+							searchCtrl.allUserCnt++;
+							
+							searchCtrl.lessThan = objects[i].updatedAt;
+						}
+						
+						SetPage();
+						$scope.isSearch = true;
+						$scope.$apply();
+						console.log("searchInfo = " + searchCtrl.allUserInfoArr.length);
+						
+					},
+					error: function(error) {
+						$scope.message = "Can't search User.";
+					}
+				});
+		}
 	});
 	
 	function GetTimeString(_time)
@@ -223,10 +337,5 @@
 			str = "0" + i;
 		return str;
 	}
-	
-	/*function SetDateArray()
-	{
-		searchCtrl.dayArr = new Array();
-	}*/
 	
 })();
